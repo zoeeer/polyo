c541ee7054a025797e048af51b9d500dbfffac38
zoeeer <rang.zoe1@gmail.com>
2017-03-17 19:08:22 +0800

    add tetromino example in README

--- README.md
+++ README.md
@@ -1,14 +1,33 @@
 # Polyomino Enumerator Written in Racket
 
+Supported polyomino types:
+- fixed
+- one-sided
+- free
+
 Main functions:
 
 polyomino enumeration:
 - (gen-polyo-of n 'free)
-- (gen-polyo-of n 'fixed)
-- (gen-fixed-polyo-upto n)
-- (gen-free-polyo-upto n)
+- (gen-polyo-upto n 'one-sided)
 
 visualize shapes:
 - (shape->pict a-shape)
 - (map shape->pict list-of-shapes)
 - (deepmap shape->pict nested-list-of-shapes)
+
+## Tetrominoes
+The following example (see "run.rkt") generates and visualizes the Tetrominoes as in the game:
+
+```
+#lang slideshow
+
+(require "shapes/polyo.rkt"
+         "draw/draw-shape.rkt")
+
+(define tetrominoes (gen-polyo-of 4 'one-sided))
+(map shape->pict tetrominoes)
+```
+which displays:
+
+![tetrominoes](assets/images/tetrominoes.png)
--- run.rkt
+++ run.rkt
@@ -1,48 +1,7 @@
 #lang slideshow
 
 (require "shapes/polyo.rkt"
-         "shapes/shape.rkt"
-         "draw/draw-shape.rkt"
-         "commons/helper.rkt")
-
-;; gen-fixed-polyo
-(define seq3 (gen-fixed-polyo-of 3))
-(define c5 (gen-fixed-polyo-upto 5))
-(define seq5free (gen-free-polyo-of 5))
-(define d5 (gen-free-polyo-upto 5))
-
-;; gen-free-polyo
-(define seq6 (gen-free-polyo-of 6))
-(define s12 (list-ref seq6 12))
-(define s15 (list-ref seq6 15))
-
-;; gen-polyo given type
-(define f5 (gen-polyo-of 5 'fixed))
-(define fr5 (gen-polyo-of 5 'free))
-(define fr6 (gen-polyo-of 6 'free))
-
-(map shape->pict fr5)
-
-;;
-(define (test-range func)
-  (display func)
-  (map (lambda (n)
-         (length (func n)))
-       (range 9)))
-
-(test-range gen-fixed-polyo-of)
-(test-range gen-free-polyo-of)
-
-(define (test-gen-polyo type)
-  (display type)
-  (map (lambda (n)
-         (length (gen-polyo-of n type)))
-       (range 9)))
-
-(test-gen-polyo 'free)
-(test-gen-polyo 'fixed)
-(test-gen-polyo 'one-sided)
+         "draw/draw-shape.rkt")
 
 (define tetrominoes (gen-polyo-of 4 'one-sided))
-
 (map shape->pict tetrominoes)
